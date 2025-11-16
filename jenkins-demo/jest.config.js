d2501f8ae0222892ba4c575f34e31644dee48e45
Chandula Jayathilake <114409648+ChandulaJ@users.noreply.github.com>
2025-11-16 22:19:23 +0530

    adjust threshold

--- jenkins-demo/jest.config.js
+++ jenkins-demo/jest.config.js
@@ -12,10 +12,10 @@ module.exports = {
     ],
     coverageThreshold: {
         global: {
-            branches: 70,
+            branches: 60,
             functions: 70,
-            lines: 70,
-            statements: 70
+            lines: 80,
+            statements: 80
         }
     },
     coverageReporters: [
